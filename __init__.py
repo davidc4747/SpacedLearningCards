a0fe21390c65026821f2389cfc2e3c4c202ce494
David G Chung <davidc4747@yahoo.com>
2025-04-29 12:18:00 -0700

    Remove Debugging function, use hooks as a decorator

--- __init__.py
+++ __init__.py
@@ -1,28 +1,31 @@
 import time
 import math
 import random
-
 from aqt import mw, gui_hooks
-from aqt.utils import showInfo, qconnect
 from anki.collection import Card
-from aqt.qt import *
+
 
 # Setup Hooks
 #########################################################################
 
-MIN_SPACING = 50
-MAX_SPACING = 80
-ONE_MINUTE = 60 # in seconds
+MIN_SPACING = 60
+MAX_SPACING = 90
+ONE_MINUTE = 60  # in seconds
+
+
+@gui_hooks.reviewer_did_show_question.append
 def handleReviewer(card: Card) -> None:
-    now = math.floor(time.time()) # in seconds since epoch
+    now = math.floor(time.time())  # in seconds since epoch
     totalRandomness = 0
 
     # if the current card is a 'red' card
     cardIds = mw.col.findCards("is:due is:learn")
     if card.id in cardIds:
         cards = list(map(lambda id: mw.col.getCard(id), cardIds))
-        overdueCards = list(filter(lambda c: c.due <= now + ONE_MINUTE and c.id != card.id, cards))
-        
+        overdueCards = list(
+            filter(lambda c: c.due <= now + ONE_MINUTE and c.id != card.id, cards)
+        )
+
         # Shuffle all cards that are comming up soon.
         random.shuffle(overdueCards)
         for card in overdueCards:
@@ -32,28 +35,3 @@ def handleReviewer(card: Card) -> None:
             card.due = now + totalRandomness + randomness
             card.flush()
             totalRandomness += randomness
-
-gui_hooks.reviewer_did_show_question.append(handleReviewer)
-
-
-
-
-
-# Print Learning Queue
-#########################################################################
-
-def printLearnQue() -> None:
-    cardIds = mw.col.findCards("is:due is:learn")
-    cards = list(map(lambda id: mw.col.getCard(id), cardIds))
-    cards.sort(key=lambda card: card.due) # Sort in time order
-
-    now = math.floor(time.time())
-    displayString = "Index) \t Time \t ReviewTime \t CardID\n"
-    for (i, card) in enumerate(cards):
-        displayString += f"{i}) \t {card.due - now} \t {card.due} \t {card.id}\n"
-
-    showInfo(f"""{displayString}""")
-
-action = QAction("View Learn Queue", mw)
-qconnect(action.triggered, printLearnQue)
-mw.form.menuTools.addAction(action)
